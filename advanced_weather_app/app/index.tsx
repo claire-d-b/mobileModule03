e91e3cd71a5df7da6ca326e33380e01dd1131309
claire-d-b <claire.deberny@essca.eu>
2026-07-21 18:31:09 +0200

    add imagebackground

--- advanced_weather_app/app/index.tsx
+++ advanced_weather_app/app/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import { ImageBackground } from "react-native";
 import { PaperProvider, MD3LightTheme } from "react-native-paper";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import CAppbar from "./CAppbar";
@@ -11,7 +12,13 @@ const _ = () => {
         edges={["top", "bottom", "left", "right"]}
       >
         <PaperProvider theme={MD3LightTheme}>
-          <CAppbar />
+          <ImageBackground
+            source={require("../assets/wallpaper.png")}
+            resizeMode="cover"
+            style={{ flex: 1 }}
+          >
+            <CAppbar />
+          </ImageBackground>
         </PaperProvider>
       </SafeAreaView>
     </SafeAreaProvider>
